163361656a8bcbbdf78c0256c006d4bee5bd29b0
Monika Tsvetanova <tsvetanova.monika@gmail.com>
2023-08-18 10:54:48 +0300

    Set up Blazor Server project

---
+++ OnlineStore/Data/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineStore.Data.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.CategoryProducts = new HashSet<Product>();
+        }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("categoryName")]
+        public string Name { get; set; }
+        public IEnumerable<Product> CategoryProducts { get; set; }
+    }
+}
---
+++ OnlineStore/Data/Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineStore.Data.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonPropertyName("image")]
+        public string ImageUrl { get; set; }
+
+        [JsonPropertyName("rating")]
+        public int Rating { get; set; }
+
+        [JsonPropertyName("unitInStock")]
+        public int UnitInStock { get; set; }
+
+        public bool Available
+        {
+            get
+            {
+                return this.Available;
+            }
+            private set
+            {
+                if (this.UnitInStock > 0)
+                {
+                    this.Available = true;
+                }
+                else
+                {
+                    this.Available = false;
+                }
+            }
+        }
+
+    }
+}
